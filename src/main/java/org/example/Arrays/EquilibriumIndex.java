9ae52e76ebe720a360b03ec0df596354becac6f1
Amitosh <amitosh.verma9@gmail.com>
2023-08-12 15:10:54 +0530

    best way of finding equilibrium index
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

---
+++ src/main/java/org/example/Arrays/EquilibriumIndex.java
@@ -0,0 +1,24 @@
+package org.example.Arrays;
+
+public class EquilibriumIndex {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 3, 7};
+        System.out.println(equilibriumIndx(arr));
+    }
+    static int equilibriumIndx(int[] arr) {
+        int c = 0;
+        int totalSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            totalSum += arr[i];
+        }
+        int leftSum = 0, rightSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            leftSum += arr[i];
+            rightSum = totalSum - leftSum + arr[i];
+            if (leftSum == rightSum) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
